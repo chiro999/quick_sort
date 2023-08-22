b23e23595127b5b0ca30653692a37274c4cc5d60
Nana Kwame <105567071+chiro999@users.noreply.github.com>
2023-08-22 18:41:35 +0000

    Add files via upload

---
+++ quicksort_low_pivot.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/* swap two elements */
+void swap(int* a, int* b){
+    int temp = *a;
+    *a =*b;
+    *b = temp;
+}
+
+int partition(int array[], int low, int high)
+{
+    int pivot= array[low];
+    int i = low + 1;
+
+    for (int x =  low + 1; x <= high; x++){
+        if(array[x] < pivot){
+            swap(&array[i], &array[x]);
+            i++;
+        }
+    }
+    swap(&array[low], &array[i - 1]);
+    return i - 1;
+}
+
+void quickSort(int array[], int low, int high){
+    if(low < high){
+        int pIndex =  partition(array, low, high);
+        quickSort(array, low, pIndex - 1);
+        quickSort(array, pIndex + 1, high);
+    }
+}
+
+int main(){
+    int array[] = {13, 21, 99, 7, 12, 15};
+    int n = sizeof(array) / sizeof(array[0]);
+
+    printf("Original Array: ");
+    for (int i = 0; i < n; i++){
+        printf("%d ", array[i]);
+    }
+
+    quickSort(array, 0, n - 1);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
